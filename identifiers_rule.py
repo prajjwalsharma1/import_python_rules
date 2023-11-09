9d345f4462568c891722ba97799cb9325ceb0764
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-09 22:27:50 +0530

    new rules

--- identifiers_rule.py
+++ identifiers_rule.py
@@ -7,3 +7,10 @@
 # 6. There is no length limit for Python identifiers. But not recommended to use too lengthy
 # identifiers.
 # 7. Dollor ($) Symbol is not allowed in Python.
+
+
+# 1. If identifier starts with _ symbol then it indicates that it is private
+# 2. If identifier starts with __(two under score symbols) indicating that strongly private
+# identifier.
+# 3.If the identifier starts and ends with two underscore symbols then the identifier is
+# language defined special name,which is also known as magic methods.
