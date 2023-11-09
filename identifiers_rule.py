0d5092daaeb0e573c2200259147f452e989f68b8
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-09 22:20:15 +0530

    file push

---
+++ identifiers_rule.py
@@ -0,0 +1,9 @@
+# 1. Alphabet Symbols (Either Upper case OR Lower case)
+# 2. If Identifier is start with Underscore (_) then it indicates it is private.
+# 3. Identifier should not start with Digits.
+# 4. Identifiers are case sensitive.
+# 5. We cannot use reserved words as identifiers
+#  Eg: def=10 
+# 6. There is no length limit for Python identifiers. But not recommended to use too lengthy
+# identifiers.
+# 7. Dollor ($) Symbol is not allowed in Python.
