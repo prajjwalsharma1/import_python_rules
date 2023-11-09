9a5e556d3edd5dea0126e71a68bbab90f99ef482
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-09 23:35:40 +0530

    new version

---
+++ diff_between_array_bytearray.py
@@ -0,0 +1,7 @@
+# The only allowed values for byte data type are 0 to 256. By mistake if we are trying to
+# # provide any other values then we will get value error.
+
+
+####################################
+
+#  TypeError: 'bytes' object does not support item assignment
