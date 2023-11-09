a0ee1756c7acaae95db9fd4195ad592dc9c29190
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-10 00:24:10 +0530

    new version

--- frozenset_set.py
+++ frozenset_set.py
@@ -1,2 +1,10 @@
 # It is exactly same as set except that it is immutable.
 # Hence we cannot use add or remove functions.
+list1=["one","two","three"]
+list2=["one","two","three"]
+print(id(list1))
+print(id(list2))
+print(list1 is list2) 
+print(list1 is not list2)  
+print(list1 == list2)  
+# r1 is r2 returns True if both r1 and r2 are pointing to the same object 
