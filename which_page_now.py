c8998e886567962abc2d23e3e99f48190b9306c1
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-14 18:13:40 +0530

    version change

--- which_page_now.py
+++ which_page_now.py
@@ -1,7 +1,11 @@
 # 75->
 # 142->
-def h(nam):
-    print(nam)
+def h(name):
+    print(name)
 h(name="prajjwal") # nam and name will be same
 #We can use both positional and keyword arguments simultaneously. But first we have to
 #take positional arguments and then keyword arguments,otherwise we will get 
+# default argument 
+def h(name="hello"):
+    print(name)
+h() # if we dont have argument then it will take default argument
