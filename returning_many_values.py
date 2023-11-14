a9869bccb33dc2a13ef8be40dc0e34b8c5a9a947
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-14 18:04:16 +0530

    version change

---
+++ returning_many_values.py
@@ -0,0 +1,15 @@
+#data={In other languages like C,C++ and Java, function can return atmost one value. But in
+#Python, a function can return any number of values.
+#Eg 1:
+#1) def sum_sub(a,b):
+#2) sum=a+b
+#3) sub=a-b
+#4) return sum,sub
+#5) x,y=sum_sub(100,50)
+#6) print("The Sum is :",x)
+#7) print("The Subtraction is :",y)
+#8)
+#9) Output
+#10) The Sum is : 150
+#11) The Subtraction is : 50 
+#}
