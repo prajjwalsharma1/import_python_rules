7bffcf8c6a662349c3d2349ac55ecf4d05b33691
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-16 16:14:14 +0530

    new app

--- lambda.py
+++ lambda.py
@@ -4,3 +4,10 @@ print("prajjwal_sharma")
 
 mul=lambda n,m:n*m
 print(mul(7,8))
+# Note:
+# Lambda Function internally returns expression value and we are not required to write
+# return statement explicitly.
+# Note: Sometimes we can pass function as argument to another function. In such cases
+# lambda functions are best choice.
+# We can use lambda functions very commonly with filter(),map() and reduce() functions,b'z
+# these functions expect function as argument.
