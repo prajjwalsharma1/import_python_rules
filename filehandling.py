8ee77ab36e4c3041603629412b1024c4bcf4ef1f
Prajjwal_sharma <cpalgorithm.daily1@gmail.com>
2023-11-20 16:03:52 +0530

    new version

--- filehandling.py
+++ filehandling.py
@@ -1,1 +1,11 @@
-print("hello_sharma")
+#print("hello_sharma")
+#read() To read total data from the file
+#read(n)  To read 'n' characters from the file
+#readline() To read only one line
+#readlines() To read all lines into a list
+
+# file exit or not os.path.isfile(file naname)
+import os 
+from os import *
+
+print(os.path.isfile("sum1.py"))
